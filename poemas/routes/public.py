7990212ae6c101e53efe0bc77ea578f55204cf61
Gabriel <gabrielmir.job@gmail.com>
2025-08-28 19:54:53 -0300

    Completo

--- poemas/routes/public.py
+++ poemas/routes/public.py
@@ -1,4 +1,6 @@
-from flask import render_template, request, redirect, url_for, session, current_app
+from flask import render_template, request, redirect, url_for, session, current_app, flash
+from flask_mail import Message
+from extensions import mail
 from . import bp
 from ..models import obtener_poemas
 
@@ -46,3 +48,30 @@ def filtrar():
 
 
     return render_template('fragmentos/poemas.html', poemas=poemas)
+
+
+@bp.route('/contact', methods=["GET", "POST"])
+def contact():
+    if request.method=="POST":
+        name = request.form.get("name")
+        email = request.form.get('email')
+        msj = request.form.get('msj')
+
+        msg = Message(subject=f"Mensaje de {name}", sender=email, recipients=['ggabrielmmiranda.20@gmail.com'])
+        msg.body= f"""De: {name} <{email}> 
+        Mensaje: {msj}
+        """
+        msg.reply_to = email  # así podés responderle directo
+
+        try:
+            mail.send(msg)
+            flash("¡Tu mensaje fue enviado con éxito!", "success")
+        except Exception as e:
+            flash("Hubo un error al enviar el mensaje", "danger")
+            print("Error:", e)
+
+        return redirect(url_for("poemas.contact"))
+
+    return render_template("public/contact.html")
+
+
